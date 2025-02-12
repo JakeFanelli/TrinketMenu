f6aa6e6716bee2e28ecbbf4883bdc8b6a0668535
Jake Fanelli <31941477+JakeFanelli@users.noreply.github.com>
2025-02-12 10:42:34 -0500

    fix: exact trinket name matching
    current implementation has an issue when an item in your bag shares a similar name as your trinket. Convert to using exact matching to avoid this.

--- TrinketMenu.lua
+++ TrinketMenu.lua
@@ -414,18 +414,22 @@ end
 
 function TrinketMenu.FindItem(name, includeInventory)
 	if includeInventory then
-		for i = 13, 14 do
-			if string.find(GetInventoryItemLink("player", i) or "", name, 1, true) then
-				return i
+			for i = 13, 14 do
+					local itemLink = GetInventoryItemLink("player", i) or ""
+					local itemName = GetItemInfo(itemLink)
+					if itemName == name then
+							return i
+					end
 			end
-		end
 	end
 	for i = 0, 4 do
-		for j = 1, TrinketMenu.GetContainerNumSlots(i) do
-			if string.find(TrinketMenu.GetContainerItemLink(i, j) or "", name, 1, true) then
-				return nil, i, j
+			for j = 1, TrinketMenu.GetContainerNumSlots(i) do
+					local containerItemLink = TrinketMenu.GetContainerItemLink(i, j) or ""
+					local containerItemName = GetItemInfo(containerItemLink)
+					if containerItemName == name then
+							return nil, i, j
+					end
 			end
-		end
 	end
 end
 
